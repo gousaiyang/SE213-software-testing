2c03433f10f7ca9dfe6d25e57a273a5b81e2c3cc
Chuzhe Tang <t.chuzhe@qq.com>
2018-05-20 19:49:10 +0800

    some tests, some bugs

--- project3/test_bookstore.py
+++ project3/test_bookstore.py
@@ -5,7 +5,11 @@ import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 # Load all config variables into the global namespace.
@@ -14,6 +18,11 @@ with open('config.json', 'r') as f:
 
 
 class TestBookStore(unittest.TestCase):
+    usr_uname = 'testuser'
+    usr_pwd = '000000'
+    admin_uname = 'admin'
+    admin_pwd = 'NF3z075ZCqmAiQCP'
+    
     @classmethod
     def setUpClass(cls):
         '''Initialize the web driver only once.
@@ -21,11 +30,89 @@ class TestBookStore(unittest.TestCase):
         cls.driver = webdriver.Chrome(webdriver_path)
         cls.driver.maximize_window()
         cls.driver.implicitly_wait(webdriver_wait_time)
+        cls.wait = WebDriverWait(cls.driver, webdriver_wait_time)
 
-    def test_xxx(self):
-        '''Test xxx feature of the book store.'''
+    def setUp(self):
+        # load web page
         self.driver.get(target_url)
-        time.sleep(5)
+        time.sleep(2)
+
+    def test_user_login(self):
+        '''Test xxx feature of the book store.'''
+        # trigger login
+        login_btn = self.driver.find_element_by_xpath('/html/body/nav[1]/div/div[2]/ul/li[2]/a')
+        login_btn.click()
+        # fill-in form, submit
+        form_uname = self.driver.find_element_by_xpath('//*[@id="loginUsername"]')
+        EC.visibility_of(form_uname)
+        form_uname.send_keys(self.usr_uname)
+        form_pwd = self.driver.find_element_by_xpath('//*[@id="loginPassword"]')
+        form_pwd.send_keys(self.usr_pwd)
+        submit_btn = self.driver.find_element_by_xpath('//*[@id="btnLogin"]')
+        submit_btn.click()
+        # check username display
+        dsp_uname = self.driver.find_element_by_xpath('//*[@id="myNavbarNickname"]')
+        self.assertEqual(dsp_uname.text, "测试用户")
+        # logout
+        btn = self.driver.find_element_by_xpath('/html/body/nav[1]/div/div[2]/ul/li[2]/a')
+        btn.click()
+        logout_btn = self.driver.find_element_by_xpath('/html/body/nav[1]/div/div[2]/ul/li[2]/ul/li[6]/a')
+        logout_btn.click()
+
+    def test_search_book(self):
+        # choose a catagory
+        select = Select(self.driver.find_element_by_xpath('//*[@id="selectCategory"]'))
+        select.select_by_visible_text('计算机')
+        kwd = self.driver.find_element_by_xpath('//*[@id="searchBookInput"]')
+        kwd.clear()
+        kwd.send_keys('Java')
+        # check result
+        result = self.driver.find_element_by_xpath('//*[@id="book-4"]/div/div/h4')
+        self.assertEqual(result.text, 'Java编程思想')
+        
+    def test_purchase(self):
+        # trigger login
+        self.driver.find_element_by_xpath('/html/body/nav[1]/div/div[2]/ul/li[2]/a').click()
+        # fill-in form, submit
+        self.driver.find_element_by_xpath('//*[@id="loginUsername"]').send_keys(self.usr_uname)
+        self.driver.find_element_by_xpath('//*[@id="loginPassword"]').send_keys(self.usr_pwd)
+        self.driver.find_element_by_xpath('//*[@id="btnLogin"]').click()
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="myNavbarNickname"]')))  # till username is displayed
+        # check cart before adding
+        title = self.driver.find_element_by_xpath('//*[@id="book-1"]/div/div/h4').text
+        self.driver.find_element_by_xpath('/html/body/nav[1]/div/div[2]/ul/li[1]/a').click()
+        time.sleep(2)  # wait for ajax
+        for i, _ in enumerate(self.driver.find_elements_by_xpath('/html/body/div[1]/div/table/tbody/tr')):
+            # if present, delete it
+            if self.driver.find_element_by_xpath('/html/body/div[1]/div/table/tbody/tr[{}]/td[1]'.format(i+1)).text == title:
+                self.driver.find_element_by_xpath('/html/body/div[1]/div/table/tbody/tr[{}]/td[5]/button[2]'.format(i+1)).click()
+                self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="btnDeleteItem"]'))).click()
+                self.wait.until(EC.invisibility_of_element_located((By.XPATH, '//*[@id="btnDeleteItem"]')))
+        self.driver.back()
+        # trigger book detail modal, and add to cart
+        self.driver.find_element_by_xpath('//*[@id="book-1"]/div/div/button').click()
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="btnAddToCart"]'))).click()
+        # check cart
+        self.driver.get(target_url)
+        self.driver.find_element_by_xpath('/html/body/nav[1]/div/div[2]/ul/li[1]/a').click()
+        time.sleep(2)  # wait for ajax
+        added = False
+        for i, _ in enumerate(self.driver.find_elements_by_xpath('/html/body/div[1]/div/table/tbody/tr')):
+            if self.driver.find_element_by_xpath('/html/body/div[1]/div/table/tbody/tr[{}]/td[1]'.format(i+1)).text == title:
+                added = True
+        self.assertTrue(added)
+        # checkout
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/button'))).click()
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="btnPay"]'))).click()
+        self.assertEqual(self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="payStatus"]/div'))).text, '支付成功！')
+        self.wait.until(EC.invisibility_of_element_located((By.XPATH, '//*[@id="payDialog"]/div/div')))
+        # logout
+        self.wait.until(EC.url_changes)
+        btn = self.driver.find_element_by_xpath('/html/body/nav[1]/div/div[2]/ul/li[2]/a')
+        btn.click()
+        logout_btn = self.driver.find_element_by_xpath('/html/body/nav[1]/div/div[2]/ul/li[2]/ul/li[6]/a')
+        logout_btn.click()
+
 
     @classmethod
     def tearDownClass(cls):
